cf4f2088ca4ed79420ec2270ec7daa5ae4ebf715
phobyjun <phobyjun@gmail.com>
2022-01-15 01:14:52 +0900

    ADD: main.tf

--- output.tf
+++ output.tf
@@ -18,3 +18,21 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  vpc_name = "${var.env_name} ${var.vpc_name}"
+  cluster_name = "${var.cluster_name}-${var.env_name}"
+}
+
+## AWS VPC definition
+resource "aws_vpc" "main" {
+  cidr_block = var.main_vpc_cidr
+  tags = {
+      "Name" = local.vpc_name,
+      "kubernetes.io/cluster/${local.cluster_name}" = "shared",
+  }
+}
