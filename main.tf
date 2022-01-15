57e8bbe77b20f557c0d31815d44d523328390403
phobyjun <phobyjun@gmail.com>
2022-01-16 01:24:39 +0900

    ADD: igw configuration

--- main.tf
+++ main.tf
@@ -76,3 +76,35 @@ resource "aws_subnet" "private-subnet-b" {
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
+
+# Internet gateway and routing tables for public subnets
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    "Name" = "${local.vpc_name}-public-route"
+  }
+}
+
+resource "aws_route_table_association" "public-a-association" {
+  subnet_id      = aws_subnet.public-subnet-a.id
+  route_table_id = aws_route_table.public-route.id
+}
+
+resource "aws_route_table_association" "public-b-association" {
+  subnet_id      = aws_subnet.public-subnet-b.id
+  route_table_id = aws_route_table.public-route.id
+}
